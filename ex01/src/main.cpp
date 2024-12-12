84173f2cf94d718d087db7c060ae450106a1897d
Nassim Yacoubi <nassim.yacoubi@gmail.com>
2024-12-12 13:53:59 +0100

    fix

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -22,5 +22,6 @@ int main( void )
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
 	return 0;
 }
